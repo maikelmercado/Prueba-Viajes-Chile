53b7de1cabc9ba16c03f59ce388f17232ee679f4
maik mercado <maikmercado@MacBook-Pro-de-maik.local>
2024-07-23 00:13:39 -0400

    index, corriengiendo general

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(
     function(){
-        // doble click desaparece
-        $("p").dblclick(
-            function(){
-                $(this).hide("slow");     
-            }
-        )
-     
+      
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+          return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+        $('#enviarCorreo').click(function () {
+            alert("El correo fue enviado correctamente")
+        });
+
+        
+
   
         
 });
+
